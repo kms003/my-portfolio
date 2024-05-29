0c85d1e4893b67e1fe2f8b346d0c56801280529b
Kalimuthu M <kmskali003@gmail.com>
2024-05-29 20:13:21 +0530

    updates

--- main.js
+++ main.js
@@ -39,40 +39,7 @@ window.onscroll = () =>{
       menuIcon.classList.remove('fa-xmark');
       navbar.classList.remove('active');
 
-};
-
-                        // =================
-
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('header nav a');
-
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop -150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
-
-//         if(top >= offset && top < offset + height){
-//             navLinks.forEach.apply(link => {
-//                 link.classList.remove('active');
-                // document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-    //             document.querySelector(`header nav a[href='#${id}']`).classList.add('active');
-    //         });
-    //     };
-    // });
-
-                            /*  Sticky navbar */
-
-    // let header = document.querySelector('header');
-    // header.classList.toggle('sticky', window.scrollY > 100);
-
-                            /* Remove toggle icon and navbar */
-
-//     menuIcon.classList.remove('fa-xmark');
-//     navbar.classList.remove('active');
-
-// };
+};                       
 
                             /*  Scroll Reveal */
 ScrollReveal({  
@@ -89,7 +56,7 @@ ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
                             /* Typed JS*/
 
 const typed  = new Typed('.multiple-text', {
-    strings: ['Java Developer', 'Web Developer','Programmer'],
+    strings: ['Java Developer', 'Programmer'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
